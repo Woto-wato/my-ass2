2166f09c568c8ff1c9488a7f33d79ee105e4fbcb
wotowato <watowoto@gmail.com>
2021-02-28 17:18:34 +0300

    first commit

---
+++ src/Wato2.kt
@@ -0,0 +1,32 @@
+fun main() {
+    println("Hello wato")
+    division()
+    add()
+    me()
+
+}
+fun wato() {
+    println("Hello wato")
+
+}
+fun division() {
+    var num1=20
+    var num2=10
+    var sum=num1/num2
+    println(sum)
+
+}
+fun add() {
+    var num3=15
+    var num4=25
+    var num5=22
+    var num6=16
+    var sum=num3+num4+num5+num6
+    println(sum)
+
+}
+fun me() {
+    var me="wato is short"
+    println(me)
+
+}
